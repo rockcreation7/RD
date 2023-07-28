32e762cd19657793c2a709135561fab278d8e800
Rock <rockcreation7@gmail.com>
2023-07-28 22:14:43 +0100

    generic 2

--- generic/main.go
+++ generic/main.go
@@ -1,18 +1,17 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
-type sdkI[T any] interface {
-	transfer(v string) T
+type cpuI[T any] interface {
+	attack(v string) T
 }
 
-type sdk struct {
+type cpu struct {
 }
 
-func (sdk sdk) transfer(v string) string {
+func (cpu cpu) attack(v string) string {
 	return v
 }
 
@@ -21,17 +20,28 @@ type Request struct {
 	amount float64
 }
 
-type gc[T any] struct {
-	common commonAction[T]
-	sdk    sdkI[T]
+type robotI interface {
+	recharge(request Request) string
 }
 
-func (gc gc[string]) recharge(request Request) string {
-	action := newCommonAction[string]()
+type robota struct {
+	cpu    cpu
+	action commonAction[string]
+}
+
+func (robota robota) recharge(request Request) string {
+	/// action := newCommonAction[string]()
 	// s := gc.common.rechargeCommon("s", gc.sdk)
-	s := action.rechargeCommon("s", gc.sdk)
+	s := robota.action.rechargeCommon("s", robota.cpu)
 
 	return s
+}
+
+func initgc() robotI {
+	return robota{
+		action: commonAction[string]{},
+		cpu:    cpu{},
+	}
 }
 
 type commonAction[T any] struct {
@@ -41,8 +51,8 @@ func newCommonAction[T any]() commonAction[T] {
 	return commonAction[T]{}
 }
 
-func (common commonAction[T]) rechargeCommon(user string, sdk sdkI[T]) T {
-	v := sdk.transfer(user)
+func (common commonAction[T]) rechargeCommon(user string, sdk cpuI[T]) T {
+	v := sdk.attack(user)
 	switch any(v).(type) {
 	case string:
 		fmt.Println("Hello")
@@ -50,11 +60,12 @@ func (common commonAction[T]) rechargeCommon(user string, sdk sdkI[T]) T {
 	return v
 }
 
-func rechargeSDK() (string, error) {
-	return "", errors.New("TEST")
-}
-
 func main() {
-
+	gcI := initgc()
+	res := gcI.recharge(Request{
+		user:   "peter",
+		amount: 10,
+	})
+	fmt.Println(res, "--- RES --- ")
 	// recharge()
 }
